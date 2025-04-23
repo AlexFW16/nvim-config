5260d3b7c9341e5281034e382600cedcefd2d55b
Alexander Peherstorfer <k12105344@students.jku.at>
2025-04-23 19:01:02 +0200

    added mappings for setup for compac

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -21,6 +21,7 @@ return {
           -- tables with the `name` key will be registered with which-key if it's installed
           -- this is useful for naming menus
           ["<Leader>b"] = { name = "Buffers" },
+          ["<Leader>z"] = { name = "Custom Mappings"},
           -- quick save
           -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
           -- Run menu
@@ -29,11 +30,20 @@ return {
           ["<Leader>rp"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "python3 " .. vim.fn.expand("%"), direction = "float" }):toggle() end, desc = "Run current file with Python 3" },
           ["<Leader>rn"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "node " .. vim.fn.expand("%"), direction = "float" }):toggle() end, desc = "Run current file with Node.js" },
           ["<Leader>rb"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "make", direction = "float" }):toggle() end, desc = "Run make" },
+
+          -- Custom Keymaps
+          ["<Leader>zs"] = { function()  vim.cmd("vsplit")
+            vim.cmd("split")
+            vim.cmd("resize 30")
+            vim.cmd("terminal bash -i -c 'source ~/.bashrc && source venv/bin/activate && exec bash -li'") -- Opens a terminal and sources the venv, keeping the basrc setup
+
+            end,
+            desc = "COMPAC workspace setup" }, -- setup for COMPAC
         },       t = {
           -- setting a mapping to false will disable it
           ["<esc>"] = false,
         },
-      },
+     },
     },
   },
 }
