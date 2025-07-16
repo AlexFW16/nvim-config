5958dd326a99dfc7ae1e9fc23be8fc171620ab0a
AlexFW <peherstorfer.alexander@gmail.com>
2025-07-16 12:35:02 +0200

    added mappings

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -1,4 +1,3 @@
-
 return {
   {
     "AstroNvim/astrocore",
@@ -12,39 +11,72 @@ return {
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
           ["<Leader>bD"] = {
             function()
-              require("astroui.status").heirline.buffer_picker(function(bufnr)
-                require("astrocore.buffer").close(bufnr)
-              end)
+              require("astroui.status").heirline.buffer_picker(
+                function(bufnr) require("astrocore.buffer").close(bufnr) end
+              )
             end,
             desc = "Pick to close",
           },
           -- tables with the `name` key will be registered with which-key if it's installed
           -- this is useful for naming menus
           ["<Leader>b"] = { name = "Buffers" },
-          ["<Leader>z"] = { name = "Custom Mappings"},
+          ["<Leader>z"] = { name = "Custom Mappings" },
           -- quick save
           -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
           -- Run menu
           ["<Leader>r"] = { name = "Run" },
-          ["<Leader>rf"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "python " .. vim.fn.expand("%"), direction = "float" }):toggle() end, desc = "Run current file" },
-          ["<Leader>rp"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "python3 " .. vim.fn.expand("%"), direction = "float" }):toggle() end, desc = "Run current file with Python 3" },
-          ["<Leader>rn"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "node " .. vim.fn.expand("%"), direction = "float" }):toggle() end, desc = "Run current file with Node.js" },
-          ["<Leader>rb"] = { function() require("toggleterm.terminal").Terminal:new({ cmd = "make", direction = "float" }):toggle() end, desc = "Run make" },
+          ["<Leader>rf"] = {
+            function()
+              require("toggleterm.terminal").Terminal
+                :new({ cmd = "python " .. vim.fn.expand "%", direction = "float" })
+                :toggle()
+            end,
+            desc = "Run current file",
+          },
+          ["<Leader>rp"] = {
+            function()
+              require("toggleterm.terminal").Terminal
+                :new({ cmd = "python3 " .. vim.fn.expand "%", direction = "float" })
+                :toggle()
+            end,
+            desc = "Run current file with Python 3",
+          },
+          ["<Leader>rn"] = {
+            function()
+              require("toggleterm.terminal").Terminal
+                :new({ cmd = "node " .. vim.fn.expand "%", direction = "float" })
+                :toggle()
+            end,
+            desc = "Run current file with Node.js",
+          },
+          ["<Leader>rb"] = {
+            function() require("toggleterm.terminal").Terminal:new({ cmd = "make", direction = "float" }):toggle() end,
+            desc = "Run make",
+          },
 
           -- Custom Keymaps
-          ["<Leader>zs"] = { function()  vim.cmd("vsplit")
-            vim.cmd("split")
-            vim.cmd("resize 30")
-            vim.cmd("terminal bash -i -c 'source ~/.bashrc && source venv/bin/activate && exec bash -li'") -- Opens a terminal and sources the venv, keeping the basrc setup
-
+          ["<Leader>zs"] = {
+            function()
+              vim.cmd "vsplit"
+              vim.cmd "split"
+              vim.cmd "resize 30"
+              vim.cmd "terminal bash -i -c 'source ~/.bashrc && source venv/bin/activate && exec bash -li'" -- Opens a terminal and sources the venv, keeping the basrc setup
             end,
-            desc = "COMPAC workspace setup" }, -- setup for COMPAC
-        },       t = {
+            desc = "COMPAC workspace setup",
+          }, -- setup for COMPAC
+
+          ["<Leader>zf"] = { function() vim.lsp.buf.format { async = true } end, desc = "LSP: Format file" }, -- Auto formats the file
+          ["<Leader>zl"] = {
+            function() require("nabla").popup { border = "rounded" } end,
+            desc = "Render inline latex",
+          },
+        },
+
+        t = {
           -- setting a mapping to false will disable it
           ["<esc>"] = false,
         },
-     },
+      },
     },
   },
 }
-
